e0803d8df92cc365c79314c7e8849c0ccacf9fce
王靖 <445091571@qq.com>
2018-12-18 09:49:30 +0800

    初次提交，将文件加进版本库

---
+++ src/main/java/com/bootExample/bootDdemo/BootDdemoApplication.java
@@ -0,0 +1,14 @@
+package com.bootExample.bootDdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDdemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/bootExample/bootDdemo/BootDdemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bootExample.bootDdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootDdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
